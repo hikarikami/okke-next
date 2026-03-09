596f472b909d12fe67888202581e0fd734a1db06
hikarikami <r.w.keon@gmail.com>
2026-03-09 19:00:04 +1030

    add turso config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv';
+
+config();
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-	dialect: 'sqlite',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dialect: 'turso',
+	dbCredentials: { url: process.env.DATABASE_URL, authToken: process.env.DATABASE_AUTH_TOKEN },
 	verbose: true,
 	strict: true
 });
